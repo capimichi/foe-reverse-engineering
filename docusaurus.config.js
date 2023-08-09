921c62761eb9b771242a8c60ebdd59542c9ff353
Michele Capichioni <capimichi@gmail.com>
2023-08-09 11:29:20 +0200

    Modifiche

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,20 +6,22 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Forge Of Empires - Reverse Engineering',
+  tagline: 'This is a project to reverse engineer the game Forge Of Empires',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://capimichi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/foe-reverse-engineering/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'capimichi', // Usually your GitHub org/user name.
+  projectName: 'foe-reverse-engineering', // Usually your repo name.
+
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
